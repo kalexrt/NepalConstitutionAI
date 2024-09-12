e5a5901588d03eb97691f70a2fa6a28ad7da4800
Znis <metwayana@gmail.com>
2024-09-12 14:53:06 +0545

    feat: set up the postgres db

---
+++ nepal_constitution_ai/config/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    LANGSMITH_API_KEY: str
+    PINECONE_API_KEY: str
+    OPENAI_API_KEY: str
+    EMBEDDING_MODEL: str
+    OPENAI_MODEL: str
+    PINECONE_INDEX: str
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
+
+__all__ = ["settings"]
