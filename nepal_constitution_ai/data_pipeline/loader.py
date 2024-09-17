3b407f50c65d08cf1ecf50e2626d997a33b086a9
kalexrt <kalashestha@gmail.com>
2024-09-17 16:43:54 +0545

    feat: add exception handling for file extensions

--- nepal_constitution_ai/data_pipeline/loader.py
+++ nepal_constitution_ai/data_pipeline/loader.py
@@ -1,12 +1,20 @@
+import os
+from loguru import logger
 from langchain_community.document_loaders import PyPDFLoader
 from nepal_constitution_ai.config.config import settings
-from loguru import logger
 
 def load_pdf():
+    file_path = settings.FILE_PATH
+    file_extension = os.path.splitext(file_path)[1].lower()
+
+    if file_extension != '.pdf':
+        logger.error(f"Invalid file type: {file_extension}. Expected a .pdf file.")
+        return None
+    
     try:
-        logger.info(f"Attempting to load PDF from {settings.FILE_PATH}")
+        logger.info(f"Attempting to load PDF from {file_path}")
         loader = PyPDFLoader(
-            file_path=settings.FILE_PATH,
+            file_path=file_path,
             extract_images=True
         )
         logger.info("PDF loaded successfully")
